a6299d2e15086dc52ea7d077d869650677542253
Gutrot10 <ciprian_vranceanu@yahoo.com>
2023-10-19 15:00:21 +0300

    Lab1

---
+++ Lab1/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        int i=0;
+        System.out.println(i);
+        float x=3.211111111111111111111111111f;
+        double y=3.222222222222222222222222222222d;
+        System.out.println(x);
+        System.out.println(y);
+
+        x = (float)y;
+        Double z1 = 2.2d;
+        double z2 = 2.2d;
+        System.out.println(Double.valueOf(z2));
+    }
+}
